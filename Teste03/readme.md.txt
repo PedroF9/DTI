14fe626022822aaceddb1711eecd732068361679
PedroF9 <126017342+PedroF9@users.noreply.github.com>
2024-04-12 18:31:56 -0300

    Add files via upload

---
+++ Teste03/readme.md.txt
@@ -0,0 +1,10 @@
+# Teste 03
+
+### DADO QUE: 
+O usuario tentou efeturar um login na página de autenticação (criação de conta/login) com uma senha inválida
+
+### QUANDO:
+Na hora do login, o usuário insere o email de forma válida e aceita pelo sistema porém insere uma senha errada
+
+### ENTAO: 
+O sistema deverá exibir um erro de autenticação na tela
---
+++ Teste03/teste03.cy.js
@@ -0,0 +1,30 @@
+describe('Login Validation', () => {
+
+  it('Validates login via email and password', () => {
+    cy.visit('http://www.automationpractice.pl/index.php?controller=authentication');
+
+    // Type the email into the 'email address' on the sign in box
+    cy.get('input[id="email"]').type('email@test.com');//testing email
+
+    // Type the password into the 'password' on the sign in box
+    cy.get('input[id="passwd"]').type('pswordT123');//testing password
+
+    // Click on 'sign in' button
+    cy.get('button[id="SubmitLogin"]').click();
+
+    // Check if the email validation message appears
+    cy.get('.alert.alert-danger:contains("Invalid email address.")')
+    .should('not.exist')
+    .then(() => {
+      cy.log("Is a valid email");
+    });
+
+    //check if any of the password validation messages appears
+    cy.get('.alert.alert-danger:contains("Authentication")')
+    .should('exist')
+    .then(() => {
+      cy.log("Is NOT the correct password for this account");
+    });
+
+  });
+});
